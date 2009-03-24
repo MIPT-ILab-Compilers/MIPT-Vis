4ffaa2f5fafaa024683aa3f910e9ee683dbfadbb
borisshr <borisshr@fe91558a-08c0-11de-9036-69e38a880166>
2009-03-24 11:47:19 +0000

    Added Layout project to solution

---
+++ trunk/sources/Layout/layout_iface.h
@@ -0,0 +1,11 @@
+/**
+ * File: Layout/layout_iface.h - Interface of layout library,
+ * an internal representation of graphs in MiptVis tool.
+ * Copyright (C) 2009  Boris Shurygin
+ */
+#ifndef LAYOUT_IFACE_H
+#define LAYOUT_IFACE_H
+
+#include "predecls.h"
+
+#endif
