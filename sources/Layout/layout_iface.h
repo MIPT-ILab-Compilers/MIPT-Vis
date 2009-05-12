bad964fca91e255b53ec90951cc56f1af5bb5c50
borisshr <borisshr@fe91558a-08c0-11de-9036-69e38a880166>
2009-05-12 10:06:31 +0000

    Pulldown from trunk

--- sources/Layout/layout_iface.h
+++ sources/Layout/layout_iface.h
@@ -6,6 +6,35 @@
 #ifndef LAYOUT_IFACE_H
 #define LAYOUT_IFACE_H
 
-#include "predecls.h"
+#include "layout_predecls.h"
+#include "edge_aux.h"
+#include "node_aux.h"
+#include "graph_aux.h"
+
+/**
+ * Get node's corresponding auxiliary graph
+ */
+inline GraphAux * NodeAux::getGraph() const
+{
+	return static_cast< GraphAux *>( Node::getGraph());
+}
+
+inline NodeAux::NodeAux( GraphAux *graph_p, int _id):
+	Node( static_cast<Graph *>( graph_p), _id)
+	{};
+inline EdgeAux::EdgeAux( GraphAux *graph_p, int _id, NodeAux* _pred, NodeAux* _succ):
+	Edge( static_cast< Graph*>( graph_p), _id,  static_cast< Node*>( _pred),  static_cast< Node*>( _succ))
+	{};
+
+inline EdgeAux *GraphAux::createEdge( NodeAux * pred, NodeAux * succ)
+{
+	return new EdgeAux( this, incEdgeId(), pred, succ);
+};
+  
+inline NodeAux *GraphAux::createNode()
+{
+	return new NodeAux( this, incNodeId());
+}
+	
 
 #endif
