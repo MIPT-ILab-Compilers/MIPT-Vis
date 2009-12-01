c7aa2bd876000b8f96be5ee6a24ed3e2f9a0aafc
anatoly.urban <anatoly.urban@fe91558a-08c0-11de-9036-69e38a880166>
2009-12-01 09:58:44 +0000

    Added missed file

---
+++ sources/Parser/parser_iface.h
@@ -0,0 +1,9 @@
+/**
+ * File: Parser/praser_iface.h - Interface of parser tool.
+ * Copyright (C) 2009  MIPT-VIS
+ */
+#ifndef PARSER_IFACE_H_
+#define PARSER_IFACE_H_
+bool convertDumpToXML( const char * file);
+
+#endif PARSER_IFACE_H_
